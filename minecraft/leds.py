a1e1974f7dca4cbd6fb0c9678f09a8df8f8ab5d1
Steve Dalton <steve@refactor.com.au>
2013-11-18 09:02:51 +1000

    leds

--- minecraft/leds.py
+++ minecraft/leds.py
@@ -4,13 +4,14 @@ import time
 board = Arduino('/dev/ttyACM0')
 
 def rgb_cycle():
-	board.digital[13].write(1)
-	time.sleep(1)
-	board.digital[13].write(0)
 	board.digital[9].write(1)
 	time.sleep(1)
 	board.digital[9].write(0)
 	board.digital[10].write(1)
 	time.sleep(1)
+	board.digital[10].write(0)
+	board.digital[11].write(1)
+	time.sleep(1)
+	board.digital[11].write(0)
 
 rgb_cycle()
